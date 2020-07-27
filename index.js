6fb29638d5d033224f1b8e5ee62470da4c25625f
Ema <linknin3@gmail.com>
2020-07-27 16:39:24 -0600

    responsive_sidenav

---
+++ index.js
@@ -0,0 +1,35 @@
+const menubar = document.getElementById('menubars');
+const navBar = document.getElementById('navBar');
+const sideNav = document.getElementById('sideNav');
+const hidenav = document.getElementById('hidenav');
+
+//Add shadow navbar on scroll
+window.onscroll  = () => {
+    adddNavShadow();
+}
+
+// Side nav show and hide
+const sideNavToggle = ()=>{
+    menubar.firstElementChild.classList.toggle('simple');
+    sideNav.classList.toggle('active');
+    body.classList.toggle('no-scroll');
+}
+
+//Add shadow navbar fucntion
+const adddNavShadow = ()=> {
+    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
+        navBar.classList.add('navshadow');
+    }else{
+        navBar.classList.remove('navshadow');
+    }
+}
+
+// Show or hide side nav on menu-icon
+menubar.addEventListener('click', ()=>{
+   sideNavToggle();
+});
+
+// hide side nav when clikc outside
+hidenav.addEventListener('click', ()=>{
+    sideNavToggle();
+});
