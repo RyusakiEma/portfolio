3e79363f57d3894fb0e17509c4eb763a92a604db
Ema <linknin3@gmail.com>
2020-07-28 17:00:21 -0600

    section-animations

--- index.js
+++ index.js
@@ -3,12 +3,30 @@ const navBar = document.getElementById('navBar');
 const sideNav = document.getElementById('sideNav');
 const sidenavLinks = document.querySelectorAll('.sidenav-list-items')
 const hidenav = document.getElementById('hidenav');
+//SECTIONS
+const aboutSection = document.getElementById('aboutSection');
+const celularesnica = document.getElementById('celularesnica');
+const grekoostudio = document.getElementById('grekoostudio');
 
 //Add shadow navbar on scroll
 window.onscroll  = () => {
     adddNavShadow();
+
+    fadeUpScoroll(aboutSection);
+    fadeUpScoroll(celularesnica);
+    fadeUpScoroll(grekoostudio);
+    
 }
 
+// fadeup animation sections on scroll
+const fadeUpScoroll = (section)=>{
+    let sectionPosition = section.getBoundingClientRect().top;
+    let screenHeight = window.innerHeight;
+ 
+    if(sectionPosition < screenHeight){
+        section.style.animation = 'fadeUp 1.11s'
+    }
+}
 // Side nav show and hide
 const sideNavToggle = ()=>{
     menubar.firstElementChild.classList.toggle('simple');
