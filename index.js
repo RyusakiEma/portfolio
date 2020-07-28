0c82454a11c57373d0d209481e14b3ec4765fa8e
Ema <linknin3@gmail.com>
2020-07-27 22:06:52 -0600

    fadeDown-animation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const menubar = document.getElementById('menubars');
 const navBar = document.getElementById('navBar');
 const sideNav = document.getElementById('sideNav');
+const sidenavLinks = document.querySelectorAll('.sidenav-list-items')
 const hidenav = document.getElementById('hidenav');
 
 //Add shadow navbar on scroll
@@ -32,4 +33,12 @@ menubar.addEventListener('click', ()=>{
 // hide side nav when clikc outside
 hidenav.addEventListener('click', ()=>{
     sideNavToggle();
+});
+
+//hide side nav when click on links
+
+sidenavLinks.forEach(element => {
+   element.addEventListener('click', ()=>{
+        sideNavToggle();
+   });
 });
