a6bb9d23a1efd423a644a3fade3cdf0e062b7bbb
Ema <linknin3@gmail.com>
2020-07-30 17:04:35 -0600

    webpack-added

---
+++ src/app.js
@@ -0,0 +1,88 @@
+import './styles.css';
+import './images/ema.jpeg';
+import './images/external-link.svg';
+import './images/github-icon.svg';
+import './images/linkedin-icon.svg';
+import './images/paper-plain.svg';
+import './images/dashboard.png';
+import './images/grekoo1.png';
+let  screenHeight = window.innerHeight;
+const menubar = document.getElementById('menubars');
+const navBar = document.getElementById('navBar');
+const sideNav = document.getElementById('sideNav');
+const sidenavLinks = document.querySelectorAll('.sidenav-list-items')
+const hidenav = document.getElementById('hidenav');
+//SECTIONS
+const aboutSection = document.getElementById('about');
+const projectSection = document.getElementById('projects');
+const celularesnica = document.getElementById('celularesnica');
+const grekoostudio = document.getElementById('grekoostudio');
+const sectionTitles = document.querySelectorAll('.section-titles')
+
+//Add shadow navbar on scroll
+window.onscroll  = () => {
+
+    adddNavShadow();
+
+    fadeRightBorder();
+
+    fadeUpScroll(aboutSection);
+    fadeUpScroll(projectSection);
+    fadeUpScroll(celularesnica);
+    fadeUpScroll(grekoostudio);  
+
+}
+
+const getElementPosition = (element)=>{
+    return element.getBoundingClientRect().top
+}
+
+const fadeRightBorder = () =>{
+    sectionTitles.forEach(title =>{
+        let titlePosition = getElementPosition(title);
+        if(titlePosition < screenHeight){
+            title.style.setProperty('--fadeRight', 'fadeRight 800ms running');
+        } 
+    })
+}
+
+// fadeup animation sections on scroll
+const fadeUpScroll = (section)=>{
+    let sectionPosition = getElementPosition(section);
+    if(sectionPosition < screenHeight){
+        section.style.animation = 'fadeUp 1.11s'
+    }
+}
+// Side nav show and hide
+const sideNavToggle = ()=>{
+    menubar.firstElementChild.classList.toggle('simple');
+    sideNav.classList.toggle('active');
+    body.classList.toggle('no-scroll');
+}
+
+//Add shadow navbar fucntion
+const adddNavShadow = ()=> {
+    if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
+        navBar.classList.add('navshadow');
+    }else{
+        navBar.classList.remove('navshadow');
+    }
+}
+
+// Show or hide side nav on menu-icon
+menubar.addEventListener('click', ()=>{
+   sideNavToggle();
+});
+
+// hide side nav when clikc outside
+hidenav.addEventListener('click', ()=>{
+    sideNavToggle();
+});
+
+//hide side nav when click on links
+
+sidenavLinks.forEach(element => {
+   element.addEventListener('click', ()=>{
+        sideNavToggle();
+   });
+});
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const TerserJSPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: __dirname + '/docs',
+        filename: 'main.js'
+    },
+    devServer: {
+        port: 5000
+    },
+    optimization: {
+        minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+      },
+    module: {
+        rules:[
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader }, 
+                    { loader: 'css-loader' }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+                use:[
+                   {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'images/',
+                        useRelativePath:true
+                    }
+                   }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        })
+    ]
+}
